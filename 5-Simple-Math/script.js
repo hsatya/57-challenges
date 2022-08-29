d0b2d443395a9ace4775a0b276f477d59180ddd4
User <harshit.satya@perpetualblock.io>
2022-08-29 13:55:46 +0530

    added functions

--- 5-Simple-Math/script.js
+++ 5-Simple-Math/script.js
@@ -7,16 +7,21 @@ const out2 = document.getElementById("out2");
 const out3 = document.getElementById("out3");
 const out4 = document.getElementById("out4");
 
-console.log(in1.value.length === 0);
+const errorMessage = (ele, message) => {
+  ele = message;
+};
+
 const calNDisplay = () => {
   if (in1.value.length === 0 || in2.value.length === 0) {
-    err.innerHTML = "Please insert a value";
+    errorMessage(err.innerHTML, "Please insert a value");
   } else {
     const val1 = Number(in1.value);
     const val2 = Number(in2.value);
     if (isNaN(val1) || isNaN(val2) || val1 < 0 || val2 < 0) {
-      err.innerHTML =
-        "Please insert a number and number should be greater than 0";
+      errorMessage(
+        err.innerHTML,
+        "Please insert a number and number should be greater than 0"
+      );
     } else {
       const add = val1 + val2;
       const sub = val1 - val2;
